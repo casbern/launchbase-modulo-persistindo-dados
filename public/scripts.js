7d0d3a5f11a1c5be2fad00d8ee8b351e6e34cbe5
Cássia <cascrisbern@gmail.com>
2020-05-12 20:59:50 -0300

    fix to mantain the filter

--- public/scripts.js
+++ public/scripts.js
@@ -40,7 +40,8 @@ function paginate(selectedPage, totalPage) {
 }
 
 const pagination =  document.querySelector(".pagination")
-console.log(pagination)
+
+const filter = pagination.dataset.filter
 
 const page = +pagination.dataset.page
 
@@ -54,7 +55,11 @@ for(let page of pages) {
   if(String(page).includes("...")) {
     elements += `<span>${page}</span>`
   } else {
-    elements += `<a href="?page=${page}">${page}</a>`
+    if(filter) {
+      elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+    } else {
+      elements += `<a href="?page=${page}">${page}</a>`
+    }
   }
 }
 
